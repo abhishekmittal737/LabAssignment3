8d1e65314c25737f04ddbf2023296d01fce6e72c
abhishekmittal737 <56733216+abhishekmittal737@users.noreply.github.com>
2022-10-19 15:57:19 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,38 @@
+package assignment3;
+
+public class Q1 {
+	public static void main(String[] args) 
+	{
+		String first="tank";
+		String second ="plane";
+		int i=0,j=0;
+		int len1=first.length(),len2=second.length();
+		while(i!=len1 && j!=len2)
+		{
+			if((int)first.charAt(i)==(int)second.charAt(j))
+			{
+				i++;
+				j++;
+			}
+			else if((int)first.charAt(i)<(int)second.charAt(j))
+			{
+				System.out.println("Lexicographically smaller string: "+first);
+				return;
+			}
+			else
+			{
+				System.out.println("Lexicographically smaller string: "+second);
+				return;
+			}
+		}
+		if(i==len1)
+		{
+			System.out.println("Lexicographically smaller string: "+first);
+		}
+		if(j==len2)
+		{
+			System.out.println("Lexicographically smaller string: "+second);
+		}
+		
+	}
+}
